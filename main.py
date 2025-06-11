79c3579c7e9f0608cc00c84f497e8b6089fb86ee
mrDaviesKellett <mdavies@kellettschool.com>
2025-06-11 14:52:45 +0800

    dogs class example for wanho

--- main.py
+++ main.py
@@ -6,3 +6,22 @@
 # save with HK Tz commit mssg?
 
 # another change!
+
+class Dog:
+    def __init__(self,name,size):
+        self.name = name
+        self.size = size
+dogs = {}
+names = ["rex","joey"]
+
+from random import choice, randint
+
+for i in range(10):
+    name = choice(names)
+    if name not in dogs:
+        dogs[name] = Dog(name,randint(1,10))
+    else:
+        print("dog already exists")
+
+dogs["rex"].attack("claws")
+
